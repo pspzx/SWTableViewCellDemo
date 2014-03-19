d9e392299a15c75d480872e747a6edda50edc1a3
pspzx <superrockman2009@163.com>
2014-03-19 17:08:48 +0800

    Initial Commit

---
+++ SWTableViewCellDemo/SWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SWAppDelegate.h
+//  SWTableViewCellDemo
+//
+//  Created by JL on 14-3-19.
+//  Copyright (c) 2014年 ninetowns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SWTableViewCellDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SWTableViewCellDemo
+//
+//  Created by JL on 14-3-19.
+//  Copyright (c) 2014年 ninetowns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SWAppDelegate class]));
+    }
+}
